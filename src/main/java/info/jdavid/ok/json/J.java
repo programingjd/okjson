93748c8ad3ab5a8423e5d0c77d055d4ec70ed74e
Jerome DAVID <programingjd@gmail.com>
2015-09-20 16:32:37 +0200

    -

---
+++ src/main/java/info/jdavid/ok/json/J.java
@@ -0,0 +1,478 @@
+package info.jdavid.ok.json;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.squareup.moshi.JsonDataException;
+import com.squareup.moshi.JsonReader;
+
+import okio.Buffer;
+import okio.BufferedSource;
+
+import android.util.Log;
+
+final class J {
+
+  /**
+   * Converts the given String to a
+   * {@link okio.BufferedSource} and passes it to {@link #parse(BufferedSource)}.
+   * @param s the json string to parse.
+   * @return either a map representing a json object, or a list representing a json array,
+   * or even null if the string is not valid json.
+   */
+  public static Object parse(final String s) {
+    return parse(new Buffer().writeUtf8(s));
+  }
+
+  /**
+   * Converts a json string to its object representation. The representation is using
+   * {@link java.util.Map}s for json objects and {@link java.util.List}s for json arrays.
+   * @param source the json string as an okio source.
+   * @return the object representation of the json string, or null if the source is not valid json.
+   */
+  public static Object parse(final BufferedSource source) {
+    final JsonReader reader;
+    try {
+      reader = new JsonReader(source);
+    }
+    catch (final NullPointerException e) {
+      return null;
+    }
+    final JsonReader.Token token = nextToken(reader);
+    if (token == null) return null;
+    switch (token) {
+      case BEGIN_OBJECT: {
+        try {
+          reader.beginObject();
+        }
+        catch (final IOException e) {
+          logger.log(e);
+          break;
+        }
+        catch (final JsonDataException e) {
+          logger.log(e);
+          break;
+        }
+        //noinspection unchecked
+        return walk(reader, new HashMap());
+      }
+      case BEGIN_ARRAY: {
+        try {
+          reader.beginArray();
+        }
+        catch (final IOException e) {
+          logger.log(e);
+          break;
+        }
+        catch (final JsonDataException e) {
+          logger.log(e);
+          break;
+        }
+        //noinspection unchecked
+        return walkArray(reader, new ArrayList());
+      }
+    }
+    return null;
+  }
+
+  private J() {}
+
+  private interface Logger {
+    void log(final Exception e);
+  }
+
+  static final Logger logger;
+  static {
+    boolean android;
+    try {
+      Class.forName("android.util.LogPrinter");
+      android = true;
+    }
+    catch (final ClassNotFoundException e) {
+      android = false;
+    }
+    if (android) {
+       logger = new Logger() {
+         @Override public void log(final Exception e) {
+           Log.e("J", e.getMessage(), e);
+         }
+       };
+    }
+    else {
+      logger = new Logger() {
+        @Override public void log(final Exception e) { e.printStackTrace(); }
+      };
+    }
+  }
+
+  private static JsonReader.Token nextToken(final JsonReader reader) {
+    if (reader == null) return null;
+    try {
+      return reader.peek();
+    }
+    catch (final IOException e) {
+      logger.log(e);
+      return null;
+    }
+  }
+
+  private static Number nextNumber(final JsonReader reader) throws IOException, JsonDataException {
+    return stringToNumber(reader.nextString());
+
+  }
+
+  static Number stringToNumber(final String s) {
+    if (s.indexOf('.') == -1) {
+      // integer or long
+      final int n = s.length();
+      if (n < 10) {
+        return Integer.valueOf(s);
+      }
+      else if (n > 11) {
+        return Long.valueOf(s);
+      }
+      else {
+        final boolean negative = s.indexOf('-') == 0;
+        if (n == 10) {
+          if (negative) return Integer.valueOf(s);
+          final char first = s.charAt(0);
+          if (first == '1') return Integer.valueOf(s);
+          if (first > '2') return Long.valueOf(s);
+          final Long longValue = Long.valueOf(s);
+          if (longValue > Integer.MAX_VALUE) {
+            return longValue;
+          }
+          else {
+            return longValue.intValue();
+          }
+        }
+        else { // n == 11
+          if (!negative) return Long.valueOf(s);
+          final char first = s.charAt(1);
+          if (first == '1') return Integer.valueOf(s);
+          if (first > '2') return Long.valueOf(s);
+          final Long longValue = Long.valueOf(s);
+          if (longValue < Integer.MIN_VALUE) {
+            return longValue;
+          }
+          else {
+            return longValue.intValue();
+          }
+        }
+      }
+    }
+    else {
+      return Double.valueOf(s);
+    }
+  }
+
+  private static Map<String, ?> walk(final JsonReader reader, final Map<String, ?> map) {
+    JsonReader.Token token;
+    while ((token = nextToken(reader)) != null) {
+      switch (token) {
+        case NAME: {
+          final String name;
+          try {
+            name = reader.nextName();
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            break;
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            break;
+          }
+          walk(reader, map, name);
+          break;
+        }
+        case END_OBJECT: {
+          try {
+            reader.endObject();
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            break;
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            break;
+          }
+          return map;
+        }
+        default: {
+          try {
+            reader.skipValue();
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            break;
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            break;
+          }
+        }
+      }
+    }
+    return map;
+  }
+
+  private static void walk(final JsonReader reader, final Map<String, ?> map, final String name) {
+    final JsonReader.Token token = nextToken(reader);
+    if (token == null) return;
+    switch (token) {
+      case BEGIN_OBJECT: {
+        try {
+          reader.beginObject();
+        }
+        catch (final IOException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+          break;
+        }
+        catch (final JsonDataException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+          break;
+        }
+        //noinspection unchecked
+        ((Map)map).put(name, walk(reader, new HashMap()));
+        break;
+      }
+      case BEGIN_ARRAY: {
+        try {
+          reader.beginArray();
+        }
+        catch (final IOException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+          break;
+        }
+        catch (final JsonDataException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+          break;
+        }
+        //noinspection unchecked
+        ((Map)map).put(name, walkArray(reader, new ArrayList()));
+        break;
+      }
+      case NULL: {
+        try {
+          reader.nextNull();
+        }
+        catch (final IOException e) {
+          logger.log(e);
+        }
+        catch (final JsonDataException e) {
+          logger.log(e);
+        }
+        //noinspection unchecked
+        ((Map)map).put(name, null);
+        break;
+      }
+      case BOOLEAN: {
+        try {
+          //noinspection unchecked
+          ((Map)map).put(name, reader.nextBoolean());
+        }
+        catch (final IOException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+        }
+        catch (final JsonDataException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+        }
+        break;
+      }
+      case NUMBER: {
+        try {
+          //noinspection unchecked
+          ((Map)map).put(name, nextNumber(reader));
+        }
+        catch (final IOException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+        }
+        catch (final JsonDataException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+        }
+        break;
+      }
+      case STRING: {
+        try {
+          //noinspection unchecked
+          ((Map)map).put(name, reader.nextString());
+        }
+        catch (final IOException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+        }
+        catch (final JsonDataException e) {
+          logger.log(e);
+          //noinspection unchecked
+          ((Map)map).put(name, null);
+        }
+        break;
+      }
+    }
+  }
+
+  private static List<?> walkArray(final JsonReader reader, final List<?> list) {
+    JsonReader.Token token;
+    while ((token = nextToken(reader)) != null) {
+      switch (token) {
+        case END_ARRAY: {
+          try {
+            reader.endArray();
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            break;
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            break;
+          }
+          return list;
+        }
+        case BEGIN_OBJECT: {
+          try {
+            reader.beginObject();
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+            break;
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+            break;
+          }
+          //noinspection unchecked
+          ((List)list).add(walk(reader, new HashMap()));
+          break;
+        }
+        case BEGIN_ARRAY: {
+          try {
+            reader.beginArray();
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+            break;
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+            break;
+          }
+          //noinspection unchecked
+          ((List)list).add(walkArray(reader, new ArrayList()));
+          break;
+        }
+        case NULL: {
+          try {
+            reader.nextNull();
+          }
+          catch (final IOException e) {
+            logger.log(e);
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+          }
+          //noinspection unchecked
+          ((List)list).add(null);
+          break;
+        }
+        case BOOLEAN: {
+          try {
+            //noinspection unchecked
+            ((List)list).add(reader.nextBoolean());
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+          }
+          break;
+        }
+        case NUMBER: {
+          try {
+            //noinspection unchecked
+            ((List)list).add(nextNumber(reader));
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+          }
+          break;
+        }
+        case STRING: {
+          try {
+            //noinspection unchecked
+            ((List)list).add(reader.nextString());
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            //noinspection unchecked
+            ((List)list).add(null);
+          }
+          break;
+        }
+        default: {
+          try {
+            reader.skipValue();
+          }
+          catch (final IOException e) {
+            logger.log(e);
+            break;
+          }
+          catch (final JsonDataException e) {
+            logger.log(e);
+            break;
+          }
+        }
+      }
+    }
+    return list;
+  }
+
+}
+
---
+++ src/test/java/info/jdavid/ok/json/TestJ.java
@@ -0,0 +1,238 @@
+package info.jdavid.ok.json;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestJ {
+
+  @BeforeClass
+  public static void setUp() {
+    // Use the parser once to get rid of the static initializer penalty.
+    // This is done so that the first test elapsed time doesn't get artificially high.
+    try {
+      J.parse("[{}]");
+    }
+    catch (Exception ignore) {}
+  }
+
+  @Test
+  public void testNumber1() {
+    final Number n = J.stringToNumber("9");
+    assertTrue(n instanceof Integer);
+    assertEquals(9, n.longValue());
+  }
+
+  @Test
+  public void testNumber2() {
+    final Number n = J.stringToNumber("-235");
+    assertTrue(n instanceof Integer);
+    assertEquals(-235, n.longValue());
+  }
+
+  @Test
+  public void testNumber3() {
+    final Number n = J.stringToNumber("456.1");
+    assertTrue(n instanceof Double);
+    assertEquals(456.1, (Double)n, 0);
+  }
+
+  @Test
+  public void testNumber4() {
+    final Number n = J.stringToNumber("-3.E-5");
+    assertTrue(n instanceof Double);
+    assertEquals(-3E-5, (Double)n, 0);
+  }
+
+  @Test
+  public void testNumber5() {
+    final Number n = J.stringToNumber("1234567890123");
+    assertTrue(n instanceof Long);
+    assertEquals(1234567890123L, (Long)n, 0);
+  }
+
+  @Test
+  public void testNumber6() {
+    final Number n = J.stringToNumber("-12345678901");
+    assertTrue(n instanceof Long);
+    assertEquals(-12345678901L, (Long)n, 0);
+  }
+
+  @Test
+  public void testNumber7() {
+    final Number n = J.stringToNumber("-2147483649");
+    assertTrue(n instanceof Long);
+    assertEquals(-2147483649L, (Long)n, 0);
+  }
+
+  @Test
+  public void testNumber8() {
+    final Number n = J.stringToNumber("2147483647");
+    assertTrue(n instanceof Integer);
+    assertEquals(2147483647, (Integer)n, 0);
+  }
+
+  @Test
+  public void testNumber9() {
+    final Number n = J.stringToNumber("-2000000000");
+    assertTrue(n instanceof Integer);
+    assertEquals(-2000000000, (Integer)n, 0);
+  }
+
+  @Test
+  public void testNumber10() {
+    final Number n = J.stringToNumber("-2800000000");
+    assertTrue(n instanceof Long);
+    assertEquals(-2800000000L, (Long)n, 0);
+  }
+
+
+  @Test
+  public void testNumber11() {
+    final Number n = J.stringToNumber("-1000000000");
+    assertTrue(n instanceof Integer);
+    assertEquals(-1000000000, n.longValue(), 0);
+  }
+
+  @Test
+  public void testNumber12() {
+    final Number n = J.stringToNumber("-3000000000");
+    assertTrue(n instanceof Long);
+    assertEquals(-3000000000L, (Long)n, 0);
+  }
+
+  @Test
+  public void testNumber13() {
+    final Number n = J.stringToNumber("3000000000");
+    assertTrue(n instanceof Long);
+    assertEquals(3000000000L, (Long)n, 0);
+  }
+
+  @Test
+  public void testNumber14() {
+    final Number n = J.stringToNumber("1000000000");
+    assertTrue(n instanceof Integer);
+    assertEquals(1000000000, n.longValue(), 0);
+  }
+
+  @Test
+  public void testNumber15() {
+    final Number n = J.stringToNumber("2000000000");
+    assertTrue(n instanceof Integer);
+    assertEquals(2000000000, n.longValue(), 0);
+  }
+
+  @Test
+  public void testNumber16() {
+    final Number n = J.stringToNumber("2800000000");
+    assertTrue(n instanceof Long);
+    assertEquals(2800000000L, (Long)n, 0);
+  }
+
+  @Test
+  public void testParseEmpty() {
+    final String str = "";
+    final Object parsed = J.parse(str);
+    assertNull(parsed);
+  }
+
+  @Test
+  public void testParseInvalidJson() {
+    final String str = "\"key\":\"value\"";
+    final Object parsed = J.parse(str);
+    assertNull(parsed);
+  }
+
+  @Test
+  public void testParseJsonObject0() {
+    final String str = "{}";
+    final Object parsed = J.parse(str);
+    assertTrue(parsed instanceof Map);
+    //noinspection unchecked
+    final Map<String, ?> map = (Map<String, ?>)parsed;
+    assertEquals(0, map.size());
+  }
+
+  @Test
+  public void testParseJsonArray0() {
+    final String str = "[]";
+    final Object parsed = J.parse(str);
+    assertTrue(parsed instanceof List);
+    //noinspection unchecked
+    final List<?> list = (List<?>)parsed;
+    assertEquals(0, list.size());
+  }
+
+  @Test
+  public void testParseJsonObject1() {
+    final String str = "{\"root\":{}}";
+    final Object parsed = J.parse(str);
+    assertTrue(parsed instanceof Map);
+    //noinspection unchecked
+    final Map<String, ?> map = (Map<String, ?>)parsed;
+    assertEquals(1, map.size());
+    final Object rootValue = map.get("root");
+    assertNotNull(rootValue);
+    assertTrue(rootValue instanceof Map);
+    //noinspection unchecked
+    final Map<String, ?> rootMap = (Map<String, ?>)rootValue;
+    assertEquals(0, rootMap.size());
+  }
+
+  @Test
+  public void testParseJsonArray1() {
+    final String str = "{\"root\":[]}";
+    final Object parsed = J.parse(str);
+    assertTrue(parsed instanceof Map);
+    //noinspection unchecked
+    final Map<String, ?> map = (Map<String, ?>)parsed;
+    assertEquals(1, map.size());
+    final Object rootValue = map.get("root");
+    assertNotNull(rootValue);
+    assertTrue(rootValue instanceof List);
+    //noinspection unchecked
+    final List<?> rootList = (List<?>)rootValue;
+    assertEquals(0, rootList.size());
+  }
+
+  @Test
+  public void testParseJsonObject2() {
+    final String str = "{\"a\":[1,2,3,4],\"b\":{\"b1\":\"text\",\"b2\":{}},\"c\":2.55,\"d\":true}";
+    final Object parsed = J.parse(str);
+    assertTrue(parsed instanceof Map);
+    //noinspection unchecked
+    final Map<String, ?> map = (Map<String, ?>)parsed;
+    assertEquals(4, map.size());
+    final Object aValue = map.get("a");
+    assertNotNull(aValue);
+    assertTrue(aValue instanceof List);
+    //noinspection unchecked
+    final List<?> aList = (List<?>)aValue;
+    assertEquals(4, aList.size());
+    assertEquals(1, aList.get(0));
+    assertEquals(2, aList.get(1));
+    assertEquals(3, aList.get(2));
+    assertEquals(4, aList.get(3));
+    final Object bValue = map.get("b");
+    assertNotNull(bValue);
+    assertTrue(bValue instanceof Map);
+    //noinspection unchecked
+    final Map<String, ?> bMap = (Map<String, ?>)bValue;
+    assertEquals(2, bMap.size());
+    assertEquals("text", bMap.get("b1"));
+    final Object b2Value = bMap.get("b2");
+    assertNotNull(b2Value);
+    assertTrue(b2Value instanceof Map);
+    //noinspection unchecked
+    final Map<String, ?> b2Map = (Map<String, ?>)b2Value;
+    assertEquals(0, b2Map.size());
+    assertEquals(2.55, map.get("c"));
+    assertEquals(true, map.get("d"));
+  }
+
+}
